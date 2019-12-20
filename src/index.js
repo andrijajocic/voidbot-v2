678daa6ca4ecdcf34b052e8184713e85fb70d944
salty.biscuits <emperor.yoshi66@gmail.com>
2019-12-20 08:23:21 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+/*
+    Voidbot v2 //not necesarrily.
+    Licensed under the BSD license.
+*/
+
+//Import Discord.js, note that the client is called cl here.
+const Discord = require("discord.js");
+const cl = new Discord.client();
+
+
+//TODO: let the server admin change the command prefix
+let cmdSymbol = "@";
+let ping = cl.ping();
+let uptime = cl.uptime();
+
+cl.on("ready", () => {
+  console.log(`Logged in as ${cl.user.tag}.`);
+})
+
+cl.on("message", msg => {
+  if (msg.content === `${cmdSymbol}ping`) {
+    msg.reply(`Pong! The response time is ${ping}`);
+  }
+})
+
+cl.on("message", msg => {
+  if (msg.content === `${cmdSymbol}status`) {
+    msg.reply(`Uptime: **${uptime}**`);
+  }
+})
+
+cl.login("placeholder");
