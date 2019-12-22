958310b084d4b293757ea8f64a9736e9e118a8a5
KommissarYoshi <45338139+EmperorYoshi@users.noreply.github.com>
2019-12-22 22:44:11 +0100

    Rich embed skeleton: hopefully works

--- src/index.js
+++ src/index.js
@@ -1,50 +1,67 @@
 /*
-    Voidbot v2 //not necesarrily.
+    Voidbot v2 //not necessarily.
     Licensed under the BSD license.
 */
 
 //Import Discord.js, note that the client is called cl here.
-const Discord = require("discord.js");
+import Discord from "discord.js";
 const cl = new Discord.client();
 
 
-//TODO: let the server admin change the command prefix
+//TODO: let the server admin change the command prefix -- thanks to UtopicUnicorns, discord.js-commando will be used
 let cmdSymbol = "@";
 let ping = cl.ping();
 let uptime = cl.uptime();
 
+/*
+  Test rich embed
+*/
+const testEmbed = new Discord.RichEmbed()
+    .setColor("#198019")
+    .setTitle("Test Embed")
+    .setDescription("This is a test embed. Please stop reading this >.<");
+
 //Console check to see if the bot is working.
 cl.on("ready", () => {
-      console.log(`Logged in as ${cl.user.tag}.`);
-})
+    console.log(`Logged in as ${cl.user.tag}.`);
+});
 
 /*
   Ping command: sends delay time to server
 */
 cl.on("message", msg => {
-      if (msg.content === `${cmdSymbol}ping`) {
-            msg.reply(`Pong! The response time is ${ping}`);
-      }
-})
+    if (msg.content === `${cmdSymbol}ping`) {
+        msg.reply(`Pong! The response time is ${ping}`);
+    }
+});
 
 /*
   Uptime command: sends time since boot up to server
 */
 cl.on("message", msg => {
-      if (msg.content === `${cmdSymbol}status`) {
-            msg.reply(`Uptime: **${uptime}**`);
-      }
-})
+    if (msg.content === `${cmdSymbol}status`) {
+        msg.reply(`Uptime: **${uptime}**`);
+    }
+});
 
 /*
   Server greeter: sends a message for every single new member joining the guild.
 */
 cl.on("guildMemberAdd", member => {
-      //Sets variable for member's channel so the bot can DM said member
-      const channel = member.guild.channels.find(ch => ch.name === "member-log")
-      if (!channel) return;
-      channel.send(`Welcome to the Void Linux server, ${member}`)
-})
+    //Sets variable for member's channel so the bot can DM said member
+    const channel = member.guild.channels.find(ch => ch.name === "member-log");
+    if (!channel) return;
+    channel.send(`Welcome to the Void Linux server, ${member}! Get familiar with our rules over at #welcome.`)
+});
+
+/*
+  Test embed: tests rich embedding
+*/
+cl.on("message", msg => {
+    if (msg.content === `${cmdSymbol}testEmbed` ) {
+        msg.reply(testEmbed);
+    }
+});
 
 
 cl.login("placeholder");
